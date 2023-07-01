05a375136e259e9f69f63c26f75203af17e7232a
Saboor-Hamedi <janhamedi49@gmail.com>
2023-07-01 17:54:11 +0700

    connect external database

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-
 define("APP_PATH", __DIR__ . "/../");
 require_once __DIR__ . '/../vendor/autoload.php';
 // index.php:
@@ -9,24 +6,32 @@ $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 if (file_exists(".env")) {
     $dotenv->load();
 }
+$host = 'db'; // Docker service name for the database container
+$port = $_ENV['DB_PORT'];
+$dbName = $_ENV['MYSQL_DATABASE'];
+$username = $_ENV['MYSQL_USER'];
+$password = $_ENV['MYSQL_PASSWORD'];
+
 try {
-    // Establish PDO connection
-    $dsn = "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'];
-    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbName";
+    $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // For example, you can query a table:
-    $stmt = $pdo->query('SELECT * FROM users');
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Process each row
+    $row = $pdo->query('SELECT * FROM school.users');
+    foreach($row as $ressult){
+        echo $ressult['name'];
     }
-
-    $pdo = null;
+    echo 'Connected to the database!';
 } catch (PDOException $e) {
-    // Connection failed, handle the exception
     echo 'Connection failed: ' . $e->getMessage();
 }
+
+
+
+
+
+
+
+
 
 
 
